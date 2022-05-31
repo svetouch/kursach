69cd27a436ff1e91ce86ece496ae2a910eb3d418
svetouch <100127610+svetouch@users.noreply.github.com>
2022-05-31 15:17:52 +0300

    Add files via upload

---
+++ main.cpp
@@ -0,0 +1,46 @@
+
+#include<iostream>
+using namespace std;
+
+void doublyEven( int n )
+{
+    int arr[n][n], i, j;
+
+    for ( i = 0; i < n; i++)
+        for ( j = 0; j < n; j++)
+            arr[i][j] = (n*i) + j + 1;
+
+    for ( i = 0; i < n/4; i++)
+        for ( j = 0; j < n/4; j++)
+            arr[i][j] = (n*n + 1) - arr[i][j];
+
+    for ( i = 0; i < n/4; i++)
+        for ( j = 3 * (n/4); j < n; j++)
+            arr[i][j] = (n*n + 1) - arr[i][j];
+
+    for ( i = 3 * n/4; i < n; i++)
+        for ( j = 0; j < n/4; j++)
+            arr[i][j] = (n*n+1) - arr[i][j];
+
+    for ( i = 3 * n/4; i < n; i++)
+        for ( j = 3 * n/4; j < n; j++)
+            arr[i][j] = (n*n + 1) - arr[i][j];
+
+    for ( i = n/4; i < 3 * n/4; i++)
+        for ( j = n/4; j < 3 * n/4; j++)
+            arr[i][j] = (n*n + 1) - arr[i][j];
+
+    for (i = 0; i < n; i++)
+    {
+        for ( j = 0; j < n; j++)
+            cout << arr[i][j] << " ";
+        cout << "\n";
+    }
+}
+
+int main()
+{
+    int n = 4;
+    doublyEven(n);
+    return 0;
+}
